686c1c09beaa5380353320b8d02e0f565fedb773
Yogendhra <yogendhra009@gmail.com>
2025-07-01 16:05:32 +0530

    Insertion sort implementation with slight modifications to the loop structure and reset logic.

--- arrays/sorting/bubble_sort.java
+++ arrays/sorting/bubble_sort.java
@@ -4,8 +4,8 @@ package arrays.sorting;
 public class bubble_sort {
         public static void main(String[] args) {
         int[] arr = {-5, 2, 111, 3, 5, 6};
-        for (int i = arr.length-1; i>0; i++) {
-            for(int j=0;j<arr.length-1;j++)
+        for (int i = arr.length-1; i>=0; i--) {
+            for(int j=0;j<=i-1;j++)
             {
                 if(arr[j]>arr[j+1])
                 {
