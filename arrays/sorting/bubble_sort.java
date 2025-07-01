730c8d79b4e8b232000167dfc907af24400da86a
Yogendhra <yogendhra009@gmail.com>
2025-07-01 11:59:14 +0530

    Java: Add bubble sort and selection sort implementations

--- arrays/sorting/bubble_sort.java
+++ arrays/sorting/bubble_sort.java
@@ -1,4 +1,23 @@
 package arrays.sorting;
 
+
 public class bubble_sort {
+        public static void main(String[] args) {
+        int[] arr = {-5, 2, 111, 3, 5, 6};
+        for (int i = arr.length-1; i>0; i++) {
+            for(int j=0;j<arr.length-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        } 
+            System.out.println("The sorted array is:");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
 }
