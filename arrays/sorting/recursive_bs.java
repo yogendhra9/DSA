60260efe821e2e7e0a565700584164af074e0014
Yogendhra <yogendhra009@gmail.com>
2025-07-10 10:25:34 +0530

    recursive bubble sort implementation in Java

---
+++ arrays/sorting/recursive_bs.java
@@ -0,0 +1,35 @@
+package arrays.sorting;
+
+public class recursive_bs {
+    static void bubble_sort(int arr[],int index)
+    {
+        if(index < 0) //
+        {
+
+            return;
+        }
+        else
+        {
+            for(int i=0;i<index;i++)
+            {
+                if(arr[i]>arr[i+1])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+            bubble_sort(arr, index-1); // recursive call 
+        }
+
+     
+    }
+    public static void main(String[] args) {
+        int arr[] = {336,42,12,6543,123,112345683,0,1};
+      bubble_sort(arr, arr.length-1);
+        System.out.println("The sorted array is:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
