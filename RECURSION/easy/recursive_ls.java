2f4f196d124efa55bd77c6992aabe6175ac4cf1b
Yogendhra <yogendhra009@gmail.com>
2025-08-07 11:36:17 +0530

    recursion basic and easy questions

--- RECURSION/easy/recursive_ls.java
+++ RECURSION/easy/recursive_ls.java
@@ -3,53 +3,70 @@ package RECURSION.easy;
 import java.util.ArrayList;
 
 public class recursive_ls {
-    static int ls(int[] arr,int index,int target,int location)
+//    static int ls(int[] arr,int index,int target,int location)
+//    {
+//         //b.c
+//        if(index == arr.length)
+//        {
+//            return location;
+//        }
+//        else if(arr[index] == target) {
+//            location = index;
+//            return location;
+//        }
+//        else {
+//            return ls(arr,index+1,target,location);
+//        }
+//
+//    }
+//    //Method-1 : Declaring the list outside the function and not returning anything
+//   static ArrayList<Integer> lis = new ArrayList<>();
+//   static   void findAll(int []arr, int target, int index)
+//    {
+//        if(index == arr.length) return;
+//        else if(arr[index] == target)
+//        {
+//            lis.add(index);
+//        }
+//
+//        findAll(arr,target,index+1);
+//    }
+//    //Method-2 : Returning the arraylist in every function call and return type would be ArrayList... Works by passing the arraylist in the parameter
+//    static   ArrayList<Integer> findAll(int []arr, int target, int index, ArrayList<Integer> list)
+//    {
+//        if(index == arr.length)  return list;
+//        else if(arr[index] == target)
+//        {
+//            list.add(index);
+//        }
+//
+//      return   findAll(arr,target,index+1,list);
+//    }
+   // Method-3 : Creating a new arraylist in function body which creates a new list for every recursive call...... This is sexxy question though fucking stuck on this
+    static ArrayList<Integer> findelem(int []arr,int target,int index)
     {
-         //b.c
-        if(index == arr.length)
-        {
-            return location;
-        }
-        else if(arr[index] == target) {
-            location = index;
-            return location;
-        }
-        else {
-            return ls(arr,index+1,target,location);
-        }
-
-    }
-    //Method-1 : Declaring the list outside the function and not returning anything
-   static ArrayList<Integer> lis = new ArrayList<>();
-   static   void findAll(int []arr, int target, int index)
-    {
-        if(index == arr.length) return;
+        ArrayList<Integer> list = new ArrayList<>();
+        if(index == arr.length) return list;
         else if(arr[index] == target)
         {
-            lis.add(index);
-        }
 
-        findAll(arr,target,index+1);
-    }
-    //Method-2 : Returning the arraylist in every function call and return type would be ArrayList... Works by passing the arraylist in the parameter
-    static   ArrayList<Integer> findAll(int []arr, int target, int index, ArrayList<Integer> list)
-    {
-        if(index == arr.length)  return list;
-        else if(arr[index] == target)
-        {
-            list.add(index);
-        }
+           list.add(index);
 
-      return   findAll(arr,target,index+1,list);
+        }
+      ArrayList<Integer> ans = findelem(arr,target,index+1);
+
+        list.addAll(ans);
+        return list;
     }
 
     public static void main(String[] args) {
-        int [] arr ={3,2,1,18,18,9,0};
-        int target = 18;
-        System.out.println(ls(arr,0,target,-1));
-        findAll(arr,target,0);
-        System.out.println(lis);
-        ArrayList<Integer> list = new ArrayList<>();
-        System.out.println(findAll(arr,target,0,list));
+        int [] arr ={3,2,1,9,1,0};
+        int target = 9;
+//        System.out.println(ls(arr,0,target,-1));
+//        findAll(arr,target,0);
+//        System.out.println(lis);
+//        ArrayList<Integer> list = new ArrayList<>();
+//        System.out.println(findAll(arr,target,0,list));
+        System.out.println(findelem(arr,target,0));
     }
 }
---
+++ RECURSION/subsequence_subset/remove_char.java
@@ -0,0 +1,45 @@
+package RECURSION.subsequence_subset;
+
+public class remove_char {
+//    public static void main(String[] args) {
+//        char remove = 'a';
+//        String str = "sankalp";
+//        String ans = " ";
+//        for(int i=0;i<str.length();i++)
+//        {
+//            if(remove == str.charAt(i))
+//            {
+//                continue;
+//            }
+//            else
+//            {
+//                ans = ans+str.charAt(i);
+//            }
+//        }
+//        System.out.println(ans);
+//    }
+    static String remove_char(String str,String ans,int index,char remove)
+    {
+        if(index == str.length()) return ans; //base condition
+        else if(str.charAt(index) == remove) //recursive call
+        {
+             return remove_char(str,ans,index+1,remove);
+        }
+        else
+        {
+            ans = ans+ str.charAt(index);
+           return  remove_char(str,ans,index+1,remove);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        String str = "Sankalp";
+        String ans = "";
+        char remove = 'a';
+        int index =0;
+        System.out.println(remove_char(str,ans,index,remove));
+    }
+
+}
