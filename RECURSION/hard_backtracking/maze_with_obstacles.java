1751c462a3b0bed246e2d7b74bd72456421f2884
Yogendhra <yogendhra009@gmail.com>
2025-09-08 23:00:46 +0530

    recursion medium questions

--- RECURSION/hard_backtracking/maze_with_obstacles.java
+++ RECURSION/hard_backtracking/maze_with_obstacles.java
@@ -1,6 +1,7 @@
 package RECURSION.hard_backtracking;
 
 
+import java.util.Arrays;
 
 public class maze_with_obstacles {
     static void maze_obstacles(int r, int c,int [][]nums, String processed)
@@ -56,18 +57,73 @@ public class maze_with_obstacles {
         }
         nums[r][c]=1;
     }
+    static void print_path(int r,int c ,int [][]nums,String processed,int[][]path,int step)
+    {
+        if(r == nums.length-1 && c == nums[0].length-1)
+        {
+            path[r][c] = step;
+            System.out.println(processed);
+//            for(int i=0;i<path.length;i++)
+//            {
+//                for(int j=0;j<path[0].length;j++)
+//                {
+//                    System.out.print(path[i][j] + " ");
+//
+//                }
+//                System.out.println();
+//            }
+             for(int[] arr:path)
+             {
+                 System.out.println(Arrays.toString(arr));
+             }
+            System.out.println();
+            return ;
+        }
+        if(nums[r][c] ==0)
+        {
+            return;
+        }
+        nums[r][c] = 0;
+
+        path[r][c] = step;
+        if(r<nums.length-1)
+        {
+            print_path(r+1,c,nums,processed+'D',path,step+1);
+        }
+        if(c<nums[0].length-1)
+        {
+            print_path(r,c+1,nums,processed+'R',path,step+1);
+        }
+        if(r>0)
+        {
+            print_path(r-1,c,nums,processed+'U',path,step+1);
+        }
+        if(c>0)
+        {
+            print_path(r,c-1,nums,processed+'L',path,step+1);
+        }
+        nums[r][c]=1;
+       path[r][c]=0;
+    }
 
     public static void main(String[] args) {
         int [][] nums =
-                {{1, 0, 0, 0},
-                        {1, 1, 0, 1},
-                        {1, 1, 0, 0},
-                        {0, 1, 1, 1}};
+                {{1, 1, 1, 1},
+                        {1, 1, 1, 1},
+                        {1, 1, 1, 1},
+                        {1, 1, 1, 1}};
+        int [][] path =
+                {{0, 0, 0, 0},
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 0}};
 //        maze_obstacles(0,0,nums,"");
 //        boolean [][]maze = {
 //                {true,true,true},{true,true,true},{true,true,true}
 //        };
-        no_path_restrictions(0,0,nums,"");
+//        no_path_restrictions(0,0,nums,"");
+        print_path(0,0,nums,"",path,1);
+
 
     }
 }
